5b70326b5980dedaeb2496f0fe687564b22972db
unknown <sithachoang@gmail.com>
2025-02-25 22:41:40 +0700

    video33

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import "./components/todo/todo.css";
+import TodoNew from "./components/TodoNew";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg"; // Kiểm tra lại đường dẫn
+import MyComponent from "./components/learn/MyComponent";
+import TodoData from "./components/todo/TodoData";
+
+
+
+const App = () => {
+  
+  return (
+    <div className="todo-container"> 
+       <div className="todo-title"> Todo List </div>
+       <TodoNew/>
+       <TodoData/>
+       <div className="todo-image"> <img src={reactLogo} className="logo"/></div>
+       </div>
+  )
+}
+
+export default App;
+
---
+++ src/components/learn/MyComponent.jsx
@@ -0,0 +1,15 @@
+
+import './style.css';
+const MyComponent =  () => {
+    return (
+        <>
+       
+            <div> Chuc mung nam moi 2025</div>
+              <div className="THACHS" 
+              style={{width: "1000px", height: "100px",borderRadius: "100px"}}
+              >HOANG SI THAC</div> 
+       
+        </>
+    );
+}
+export default MyComponent;
