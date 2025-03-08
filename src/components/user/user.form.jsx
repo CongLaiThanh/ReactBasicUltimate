749cb916543a2ddd84f58f890b4e5d8341c6ad85
unknown <sithachoang@gmail.com>
2025-03-08 17:46:06 +0700

    video67

--- src/components/user/user.form.jsx
+++ src/components/user/user.form.jsx
@@ -1,22 +1,34 @@
 import { Button, Input } from "antd";
+//import Password from "antd/es/input/Password";
+import { useState } from "react";
 
 const UserForm = ()  => {
-
+  const [fullName,setFullName] =  useState("");
+  const [email,setEmail] =  useState("");
+  const [password,setPasword] =  useState("");
+  const [phone,setPhone] =  useState("");
+  const handleClick = () => {
+    console.log(">>> check state:", {fullName,email,password,phone})
+  }
   return (
            
            <div className="user-form" style={{margin: "60px 0"}}>
               <div>
                     
                     <span> FullName</span>
-                    <Input/>
+                    <Input value={fullName}
+                    onChange={(event)=> {setFullName(event.target.value)}}/>
                     <span> Email</span>
-                    <Input/>
+                    <Input value={email}
+                    onChange={(event)=> {setEmail(event.target.value)}}/>
                     <span> Password</span>
-                    <Input.Password/>
+                    <Input.Password value={password} 
+                    onChange={(event)=> {setPasword(event.target.value)}} />
                     <span> Phone Number</span>
-                    <Input/> 
+                    <Input value={phone}
+                    onChange={(event)=> {setPhone(event.target.value)}}/> 
               </div>
-              <div> <Button type="primary">Create User</Button> </div>
+              <div> <Button onClick={handleClick} type="primary">Create User</Button> </div>
            </div>
   )
 }
